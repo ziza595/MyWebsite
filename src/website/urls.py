dec8df5c2140b495a5b2f06db34a2b3eed5742d2
Abdoul Aziz BA <ziza595@gmail.com>
2021-10-15 02:00:17 +0000

    Refactoring of the main website about templates folder

--- src/website/urls.py
+++ src/website/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
+app_name = "website"
 urlpatterns = [
+    path("", views.index, name="index"),
     path('admin/', admin.site.urls),
     path('blog/', include("blog.urls")),
     path('portfolio/', include("portfolio.urls")),
